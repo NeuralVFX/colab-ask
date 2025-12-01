998a0eceb80ba2c62411e5b1eb14d6c8b8c31f97
NeuralVFX <neuralvfx@gmail.com>
2025-12-01 22:17:04 +0200

    Adding more detail

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="colab-ask",
     version="0.1.0",
@@ -7,4 +10,12 @@ setup(
     install_requires=["lisette", "mistune", "pillow", "litellm"],
     author="NeuralVFX",
     description="LLM assistant magic for Colab",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/NeuralVFX/colab-ask",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires=">=3.7",
 )
