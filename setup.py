81903dfc5ab01aeb4b75359ce9d00008834a6a5d
Neural VFX <38269894+NeuralVFX@users.noreply.github.com>
2025-12-02 12:55:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (HERE / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="colab-ask",
-    version="0.1.2",
+    version="0.1.3",
     description="An LLM assistant magic command for Google Colab notebooks.", 
     long_description=long_description,
     long_description_content_type="text/markdown",
